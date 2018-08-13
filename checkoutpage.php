7aed928c87dbb4b6606b23543c472350ac2da097
jobsworks <uchennaanih16@gmail.com>
2018-08-13 14:06:28 +0100

    added price calculation

--- checkoutpage.php
+++ checkoutpage.php
@@ -37,44 +37,48 @@
                     									<h4 class="main_question" style="color: #800080; font-size: 24px"><strong>What is your time frame and budget?</strong></h4><br>
                                                        <div class="row" >
 														   <div class="col-md-12">
-														   <div class="form-group clearfix" id="features">
-																<label class="rating_type">Simple Plan</label>
-																<input type="hidden" value="50000" id="price">
-																<span class="rating" id="rating"><label for="">&#8358;50,000</label></span>
-															</div>
+																<div class="form-group clearfix" id="features">
+																		<label class="rating_type">Simple Plan</label>
+																		<input type="hidden" value="50000" id="price" class="newfeature">
+																		<span class="rating" id="rating"><label for="">&#8358;50,000</label></span>
+																	</div>
+																	
+																	
+																	<div class="form-group clearfx">
+																		<label for="total" class="rating_type">Total</label>
+																		<span class="rating" id="rating">&#8358;<label for="" id="total"></label></span>
+																		<!-- <input type="text" class="rating" id="rating" value="&#8358;0.00" readonly> -->
+																	</div>
+															</div>					
+														</div>										
 
-															
 
 															<div>
-																<label  for="">Add more features</label>
-																<div class="row">
-																	<div class="col-md-6">
-																		<div class="form-group radio_input">
-																			<label><input type="checkbox" class="checkbox1" id="chb1" value="Logo design" name="more_features[]" >Logo design</label>
+																<label for="">Add more features</label>
+																	<div class="row">
+																		<div class="col-md-6">
+																			<div class="form-group radio_input">
+																				<label><input type="checkbox" class="checkbox1" id="chb1" data-price="500" value="Logo design" name="more_features[]" >Logo design</label>
+																			</div>
+																			<div class="form-group radio_input">
+																				<label><input type="checkbox" class="checkbox1" id="chb2" data-price="120" value="Dashboard design" name="more_features[]" >Dashboard design</label>
+																			</div>
 																		</div>
+
+
+																		<div class="col-md-6">
 																		<div class="form-group radio_input">
-																			<label><input type="checkbox" class="checkbox1" value="Dashboard design" name="more_features[]" >Dashboard design</label>
+																			<label><input type="checkbox" class="checkbox1" id="chb3" value="Satisfied" name="more_features[]" >Logo design</label>
 																		</div>
+
+																	</div>
+
 																		
-																	</div>
 
-																	<div class="col-md-6">
-																		<div class="form-group radio_input">
-																			<label><input type="checkbox" class="checkbox1" value="Satisfied" name="more_features[]" >Logo design</label>
-																		</div>
-
-																	</div>
-																</div>
-
-															</div>
-
-														   </div>
-													   	
-													   </div>
-                    									<!-- /row -->
-                                                       
-
-                                                        
+																	</div>  <!-- row -->
+															
+															</div> <!-- div -->
+															                    
 
                                                     </div>
                                                     <!-- /step -->
--- js/form_handler.js
+++ js/form_handler.js
@@ -1,14 +1,85 @@
 $(document).ready(function() {
     var rating = $('#price').val();
-     var htmltag = '<div class="form-group clearfix" id="logodes"><label class="rating_type">Logo Desigbn</label><input type="hidden" value="50000" id="logo"><span class="rating" id="rating"><label for="">&#8358;60,000</label></span> </div>';
-    //alert(rating);
+    var ftotal = parseInt(rating);
+    $('#total').text(ftotal);
+     var htmltag = '<div class="form-group clearfix" id="logodes"><label class="rating_type">Logo Design</label><input type="hidden" name="logos" value="50000" class="newfeatures" id="logo"><span class="rating" id="rating"><label for="">&#8358;50,000</label></span> </div>';
+     var chb2 = '<div class="form-group clearfix" id="dashboards"><label class="rating_type">Dashboard Design</label><input type="hidden" name="dashboard" value="120000" class="newfeatures" id="dash"><span class="rating" id="rating"><label for="">&#8358;120,000</label></span> </div>';
+    var chb3 = '<div class="form-group clearfix" id="design_logo"><label class="rating_type">Dashboard Design</label><input type="hidden" name="logo_designs" value="120000" class="newfeatures" id="logo_design"><span class="rating" id="rating"><label for="">&#8358;160,000</label></span> </div>';
+
+    
     $('#chb1').click(function(event) {
+        
+        
         if(this.checked) {
-            $(htmltag).insertAfter("#features");
+            if($('#dashboards').length) {
+                $(htmltag).insertAfter("#dashboards");
+                var dashprice = $('#dash').val();
+                var nextNum = parseInt(dashprice);
+                var logoprice = parseInt($('#logo').val());
+                var fprice = ftotal + nextNum + logoprice;
+                $('#total').text(fprice);
+               
+             }else{
+                $(htmltag).insertAfter("#features");
+                var logoprice = parseInt($('#logo').val());
+                var fprice = ftotal + logoprice;
+                $('#total').text(fprice);
+
+
+             }
+           
+            
         } else {
             $('#logodes').remove();
+            if($('#dashboards').length){
+                var dashprice = $('#dash').val();
+                var nextNum = parseInt(dashprice);
+                var fprice = ftotal + nextNum;
+                $('#total').text(fprice);
+            }else{
+                $('#total').text(ftotal);
+            }
+           
+            
+
         }
     });
+
+    $('#chb2').click(function(event) {
+        if(this.checked) {
+           if($('#logodes').length) {
+            $(chb2).insertAfter("#logodes");
+            var logoprice = parseInt($('#logo').val());
+            var dashprice = parseInt($('#dash').val());
+            var fprice = ftotal + logoprice + dashprice;
+            $('#total').text(fprice);
+           }else{
+            $(chb2).insertAfter("#features");
+            var dashprice = parseInt($('#dash').val());
+            var fprice = ftotal + dashprice;
+            $('#total').text(fprice);
+           }
+           
+           
+        } else {
+            $('#dashboards').remove();
+            if($('#logodes').length) {
+                var logoprice = parseInt($('#logo').val());
+                var fprice = ftotal + logoprice;
+                $('#total').text(fprice);
+            }else {
+                $('#total').text(ftotal);
+            }
+           
+        }
+    });
+
+
+    $('#chb3').click(function(event){
+
+    });
+
+   
 
   
     
